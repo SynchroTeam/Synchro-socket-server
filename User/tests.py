f3bf77d5b6070d6009612a3cf70de9da5c9f94df
Peter D. Maquiran <petermaquiran29@gmail.com>
2021-03-07 15:27:30 +0100

    Debugging user JWT

--- User/tests.py
+++ User/tests.py
@@ -9,15 +9,15 @@ class CreateUser(TestCase):
 
         AUTHORIZATION = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjE1MTI4NTQ2LCJqdGkiOiJiNmI5NGFiMzg0MGQ0ZjMyOWVkZjg1YWZlMmE5NGQzZSIsInVzZXJfaWQiOiIxY2ZlMDNjYy0zODY3LTQ0MzgtODZlZi03MDAyNWQxNjc2NDEiLCJQRVJNSVNJT05TIjpbXSwiVVNFUk5BTUUiOiJqYWNvYiIsIklEX0NPTlNVTUVSIjoiMWNmZTAzY2MtMzg2Ny00NDM4LTg2ZWYtNzAwMjVkMTY3NjQxIiwiRVhQIjoxNjE1MTI4NTQ2fQ.eihN8l5ZddMRPH4hu5HxvUORbMK7OEHM7HxC8prTlvs'
         ID_CHANNEL = "89edfa70-7f4c-11eb-912d-af4bf29ca96d"
-        BLUE_PRINT =  "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.e30.6_IVKSoFcLzSmvH82p1MpE2k0iIQIp9H1TErw7i4vaE"
+        # BLUE_PRINT =  "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.e30.6_IVKSoFcLzSmvH82p1MpE2k0iIQIp9H1TErw7i4vaE"
 
         self.client = Client(HTTP_AUTHORIZATION=  AUTHORIZATION)
 
         respose = self.client.post("/api/v0/user/create/",{
-            'BLUE_PRINT' : BLUE_PRINT,
             'ID_CHANNEL' : ID_CHANNEL,
             'ID_USER': '1'
         })
         
+        print(respose.json())
         self.assertTrue(respose.json()['JWT_USER'])
         self.assertEqual(respose.status_code, 200)
