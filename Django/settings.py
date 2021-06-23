052fe3f2a40d6cc7299d17d6fd52b780e24a54ef
Peter D. Maquiran <petermaquiran29@gmail.com>
2021-06-23 21:45:43 +0100

    Add static folder

--- Django/settings.py
+++ Django/settings.py
@@ -179,6 +179,6 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/static_root')
