ed64e987a701c302de1ec0b39c805847ce47dd44
Peter D. Maquiran <petermaquiran29@gmail.com>
2021-06-24 21:44:01 +0100

    Improve

--- Django/asgi.py
+++ Django/asgi.py
@@ -8,7 +8,7 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
 """
 
 import os
-
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Django.settings')
 from django.core.asgi import get_asgi_application
 
 
