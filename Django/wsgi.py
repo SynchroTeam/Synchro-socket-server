4b0cc9147056fa0976e06f0b880a93dcdfd1dceb
Peter D. Maquiran <petermaquiran29@gmail.com>
2021-06-24 21:34:53 +0100

    Improve

--- Django/wsgi.py
+++ Django/wsgi.py
@@ -10,8 +10,6 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-os.environ['DJANGO_SETTINGS_MODULE'] = 'Django.settings'
-
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Django.settings')
 
 application = get_wsgi_application()
